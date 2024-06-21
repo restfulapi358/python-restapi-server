decf951a36acd89838f5c365dbc9ddeeb4587b6b
restfulapi358 <restfulapi358@gmail.com>
2024-06-21 09:20:11 -0700

    Update main.py

--- main.py
+++ main.py
@@ -16,12 +16,8 @@ def read_root():
 
 @app.post("/shutdown")
 def shutdown():
-    def shutdown_server():
-        pid = os.getpid()
-        os.kill(pid, signal.SIGINT)
-    
-    shutdown_server()
-    return JSONResponse(content={"message": "Server is shutting down..."})
+    os.kill(os.getpid(), signal.SIGINT)
+    return {"message": "Server is shutting down..."}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
