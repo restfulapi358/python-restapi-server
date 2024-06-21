a23283c262e293341c7fcfe5cd7bedc3dafb365b
restfulapi358 <restfulapi358@gmail.com>
2024-06-21 09:32:45 -0700

    Update main.py

--- main.py
+++ main.py
@@ -20,4 +20,4 @@ def shutdown():
     return {"message": "Server is shutting down..."}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8001)
